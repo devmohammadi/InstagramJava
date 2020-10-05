8556e41feedea15524b4f4cbe01ce623cd0e4888
Fatemeh Mohammadi <79f.mohammadi@gmail.com>
2020-10-05 18:56:08 +0330

    part 3

--- app/src/main/java/com/fmohammadi/instagramjava/RegisterActivity.java
+++ app/src/main/java/com/fmohammadi/instagramjava/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.fmohammadi.instagramjava;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
@@ -9,15 +11,20 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
     private EditText userName;
@@ -26,6 +33,7 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText password;
     private Button register;
     private TextView loginUser;
+    private ProgressDialog pd;
 
     private DatabaseReference mRootRef;
     private FirebaseAuth mAuth;
@@ -45,6 +53,7 @@ public class RegisterActivity extends AppCompatActivity {
 
         mRootRef = FirebaseDatabase.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
+        pd = new ProgressDialog(this);
 
         loginUser.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,7 +75,7 @@ public class RegisterActivity extends AppCompatActivity {
                         TextUtils.isEmpty(txtEmail) ||
                         TextUtils.isEmpty(txtPassword)) {
                     Toast.makeText(RegisterActivity.this, "please enter information", Toast.LENGTH_SHORT).show();
-                } else if (txtPassword.length() > 6) {
+                } else if (txtPassword.length() < 6) {
                     Toast.makeText(RegisterActivity.this, "your password must be at least 6 characters long", Toast.LENGTH_SHORT).show();
                 } else {
                     registerUser(txtUserName, txtName, txtEmail, txtPassword);
@@ -76,10 +85,37 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     private void registerUser(final String userName, final String name, final String email, final String password) {
-        mAuth.createUserWithEmailAndPassword(email , password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+        pd.setMessage("please wait");
+        pd.show();
+        mAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
             @Override
             public void onSuccess(AuthResult authResult) {
+                HashMap<String, Object> map = new HashMap<>();
+                map.put("name", name);
+                map.put("email", email);
+                map.put("username", userName);
+                map.put("uid", mAuth.getCurrentUser().getUid());
 
+                mRootRef.child("Users").child(mAuth.getCurrentUser().getUid()).setValue(map)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    pd.dismiss();
+                                    Toast.makeText(RegisterActivity.this, "Register is Successful", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegisterActivity.this ,MainActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                pd.dismiss();
+                Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
---
+++ app/src/main/java/com/fmohammadi/instagramjava/StartActivity.java
@@ -0,0 +1,67 @@
+package com.fmohammadi.instagramjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class StartActivity extends AppCompatActivity {
+
+    private ImageView iconImage;
+    private LinearLayout linearLayout;
+    private Button register;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        iconImage = findViewById(R.id.ivIcon);
+        linearLayout = findViewById(R.id.linearLayout);
+        register = findViewById(R.id.btnRegister);
+        login = findViewById(R.id.btnLogin);
+
+        linearLayout.animate().alpha(0f).setDuration(1);
+
+        TranslateAnimation animation = new TranslateAnimation(0, 0, 0, -1000);
+        animation.setDuration(2000);
+        animation.setFillAfter(false);
+        animation.setAnimationListener(new MyAnimation());
+
+        iconImage.setAnimation(animation);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StartActivity.this , RegisterActivity.class).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+            }
+        });
+    }
+
+    private class MyAnimation implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            iconImage.clearAnimation();
+            iconImage.setVisibility(View.INVISIBLE);
+            linearLayout.animate().alpha(1f).setDuration(1000);
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    }
+}
